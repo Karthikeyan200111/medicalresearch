e3e12e3159fa0972fd827a1c07ff5fa1978a7c6c
Karthikeyan200111 <kannankarthi737@gmail.com>
2024-08-02 23:16:58 +0530

    allow

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Home from "./component/Home";
+import Home from "../component/Home";
 
 
 const ChatInterface = () => {
